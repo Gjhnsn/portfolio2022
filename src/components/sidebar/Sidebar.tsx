a0fb2187d3dbc7b0239b81a2280908de8181199d
Garrett Johnson <garrettjtrains@gmail.com>
2022-02-15 14:29:27 -0800

    feat(sidebar): sidebar structure and opening animation

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  LongLine,
+  Container,
+  IconPlaceHolder,
+  ShortLine,
+  Email,
+} from "./styles";
+
+const Sidebar = () => {
+  return (
+    <Container>
+      <LongLine />
+      <IconPlaceHolder />
+      <IconPlaceHolder />
+      <IconPlaceHolder />
+      <ShortLine />
+      <Email>garrett.bjnsn@gmail.com</Email>
+      <ShortLine />
+    </Container>
+  );
+};
+
+export default Sidebar;
