7ba380d49088a11c2d449c4f31616ea50b0f8acb
Garrett Johnson <garrettjtrains@gmail.com>
2022-04-04 10:28:33 -0700

    feat(contact): add error message, and move success message to be dependant on successful submission

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -12,35 +12,51 @@ import {
   ContactForm,
 } from "./styles";
 import emailjs from "@emailjs/browser";
-import { setTimeout } from "timers/promises";
-
-
+import { render } from "@testing-library/react";
 
 const Contact = () => {
-
-  const [successMessage, setSuccessMessage] = useState<boolean>(false)
+  const [successMessage, setSuccessMessage] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<boolean>(false);
 
   const handleContactSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(`${process.env.REACT_APP_SERVICE_ID}`, `${process.env.REACT_APP_TEMPLATE_ID}`, e.currentTarget, `${process.env.REACT_APP_USER_ID}`)
-      .then((result) => {
+    emailjs
+      .sendForm(
+        `${process.env.REACT_APP_SERVICE_ID}`,
+        `${process.env.REACT_APP_TEMPLATE_ID}`,
+        e.currentTarget,
+        `${process.env.REACT_APP_USER_ID}`
+      )
+      .then(
+        (result) => {
+          setSuccessMessage(true);
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
+          setErrorMessage(true);
           console.log(error.text);
-      });
-      e.currentTarget.reset()
-      setSuccessMessage(true);
-      window.setTimeout(() => {
-        setSuccessMessage(false)
-      }, 3000);
+        }
+      );
+    e.currentTarget.reset();
+    window.setTimeout(() => {
+      setSuccessMessage(false);
+      setErrorMessage(false);
+    }, 3000);
   };
 
+  //------------ call this under successful submission instead of at end of function
   const renderSuccessMessage = () => (
     <div>
       <p>Thanks for getting in touch!</p>
     </div>
-  )
+  );
+
+  const renderErrorMessage = () => (
+    <div>
+      <p>oops! Something went wrong.</p>
+    </div>
+  );
 
   return (
     <Layout>
@@ -52,15 +68,16 @@ const Contact = () => {
       <ContactForm onSubmit={handleContactSubmit}>
         <InputWrapper>
           <InputColumn>
-            <InputField type="text" placeholder="name" name='name' />
-            <InputField type="email" placeholder="email" name='email' />
+            <InputField type="text" placeholder="name" name="name" />
+            <InputField type="email" placeholder="email" name="email" />
           </InputColumn>
           <InputColumn>
-            <MessageBox placeholder="message" name='message' />
+            <MessageBox placeholder="message" name="message" />
           </InputColumn>
         </InputWrapper>
-        <SubmitButton type="submit" value='Send Message'></SubmitButton>
+        <SubmitButton type="submit" value="Send Message"></SubmitButton>
         {successMessage && renderSuccessMessage()}
+        {errorMessage && renderErrorMessage()}
       </ContactForm>
     </Layout>
   );
