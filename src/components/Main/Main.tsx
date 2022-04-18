d9ee9b862981735628c77828ae5eb1eb20da6588
Garrett Johnson <garrettjtrains@gmail.com>
2022-04-18 11:56:50 -0700

    docs(app): delete unused imports

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
