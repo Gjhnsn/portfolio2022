fd1b02b4af3a2376938ea153a07d145bab07b916
Garrett Johnson <garrettjtrains@gmail.com>
2022-04-18 11:53:43 -0700

    feat(hero): add parallax to elements

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
 import {
   TextContainer,
   Square1,
@@ -12,9 +12,20 @@ import {
 import { LayoutContainer } from "../../common/Layout/styles";
 
 const Hero = () => {
+  const [scrollOffset, setScrollOffset] = useState(0);
+  // ---------------------------------------------------------window.scrollY getting scroll positio
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => setScrollOffset(window.scrollY);
+
   return (
-    <section id='home'>
-      <LayoutContainer style={{marginBottom: '100px'}} className="landing">
+    <section id="home">
+      <LayoutContainer style={{ marginBottom: "100px" }} className="landing">
         <TextContainer>
           <FlexWrapper>
             <div>
@@ -26,10 +37,16 @@ const Hero = () => {
           </FlexWrapper>
         </TextContainer>
       </LayoutContainer>
-      <ImageBackground />
+      <ImageBackground
+        style={{ transform: `translateY(${scrollOffset * 0.08}px)` }}
+      />
       <SquareWrapper>
-        <Square1 />
-        <Square2 />
+        <Square1
+          style={{ transform: `translateY(-${scrollOffset * 0.5}px)` }}
+        />
+        <Square2
+          style={{ transform: `translateY(-${scrollOffset * 0.2}px)` }}
+        />
       </SquareWrapper>
     </section>
   );
